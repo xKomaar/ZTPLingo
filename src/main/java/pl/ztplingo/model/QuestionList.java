4fc6e425ef185b7ddc84475cc5e332e62b8feca3
xKomaar <jakubo148@gmail.com>
2024-01-18 14:27:18 +0100

    Dobrze ustawia listę fraz do quizu

--- src/main/java/pl/ztplingo/model/QuestionList.java
+++ src/main/java/pl/ztplingo/model/QuestionList.java
@@ -17,20 +17,33 @@ public class QuestionList {
     public QuestionList(List<Word> words, List<Sentence> sentences, int questionQuantity) {
         phraseDecoratorList = new ArrayList<>();
         Random random = new Random();
-        for(int i=0; i<questionQuantity/2 && !words.isEmpty(); i++) {
+        int bound;
+        //sprawdzamy, czy zdań jest mniej, niż połowa żądanej ilości pytań
+        //jeżeli tak, to musimy dodać więcej słów (np 6 pytań, a w bazie są tylko 2 zdania - 4 slowa, 2 zdania)
+        if(sentences.size() < questionQuantity-questionQuantity/2) {
+            bound = questionQuantity/2 + questionQuantity-questionQuantity/2 - sentences.size();
+        } else {
+            bound = questionQuantity/2;
+        }
+        //dodajemy losowe (lub wszystkie) słowa
+        for(int i=0; i<bound && !words.isEmpty(); i++) {
             int randomIndex = random.nextInt(words.size());
             Word randomWord = words.get(randomIndex);
             WordDecorator wordDecorator = new WordDecorator(randomWord);
             phraseDecoratorList.add(wordDecorator);
             words.remove(randomWord);
         }
-        for(int i=questionQuantity/2; i<questionQuantity && !sentences.isEmpty(); i++) {
+        //teraz pętla wykona się tyle razy, ile pozostało fraz do wpisania (od obecnej wielkosci listy fraz, do żądanej ilosci pytan)
+        bound = phraseDecoratorList.size();
+        //dodajemy losowe (lub wszystkie) zdania
+        for(int i=bound; i<questionQuantity && !sentences.isEmpty(); i++) {
             int randomIndex = random.nextInt(sentences.size());
             Sentence randomSentence = sentences.get(randomIndex);
             SentenceDecorator sentenceDecorator = new SentenceDecorator(randomSentence);
             phraseDecoratorList.add(sentenceDecorator);
             sentences.remove(randomSentence);
         }
+        //mieszamy listę
         Collections.shuffle(phraseDecoratorList);
     }
 
